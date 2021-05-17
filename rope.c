c9a0ffd37d52d6aa52e1c2d1fad655f513066631
mihneabranzeu <mihneacnitv@yahoo.com>
2021-05-17 22:33:35 +0300

    Primii pasi cu randu

--- rope.c
+++ rope.c
@@ -4,15 +4,40 @@
 #include <string.h>
 #include <assert.h>
 
+#include "utils.h"
+
 #define EMPTY ""
 
 
 RopeNode* makeRopeNode(const char* str) {
-    // TODO
+    // Allocate memory for a new node
+    RopeNode *new_node = malloc(sizeof(RopeNode));
+    DIE(new_node == NULL, "Unable to allocate memory for a new node!\n");
+
+    // Add the data into the node
+    new_node->str = malloc(sizeof(char) * (strlen(str) + 1));
+    DIE(new_node->str == NULL, "Unable to allocate memory for the node's string!\n");
+    new_node->str = str;
+
+    // Initialize properties
+    new_node->left = NULL;
+    new_node->right = NULL;
+    new_node->weight = 0;
+
+    return new_node;
 }
 
 RopeTree* makeRopeTree(RopeNode* root) {
-    // TODO
+    // Allocate memory for the new rope
+    RopeTree *tree = malloc(sizeof(RopeTree));
+    DIE(tree == NULL, "Unable to allocate memory for the tree!\n");
+
+    // Initialize the root
+    tree->root= malloc(sizeof(RopeNode));
+    DIE(tree->root == NULL, "Unable to allocate memory for the root!\n");
+    memcpy(tree->root, root, sizeof(RopeNode));
+
+    return tree;
 }
 
 
@@ -63,9 +88,20 @@ int getTotalWeight(RopeNode* rt) {
 
 
 RopeTree* concat(RopeTree* rt1, RopeTree* rt2) {
-    // TODO 1. Concat - 10p
-}
+    // Initialize the root node of the new tree
+    RopeNode *root = makeRopeNode(EMPTY);
+    root->left = rt1->root;
+    root->right = rt2->root;
+    root->weight = getTotalWeight(root->left);
+    
+    // Free old trees' memory
+    free(rt1);
+    free(rt2);
 
+    // Initialize the new tree
+    RopeTree *new_tree = makeRopeTree(root);
+    return new_tree;
+}
 
 
 char indexRope(RopeTree* rt, int idx) {
